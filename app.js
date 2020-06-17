64a7a2217dfd59e1837d109386d5c00bbcb28c21
shreyasaxena2k <65354886+shreyasaxena2k@users.noreply.github.com>
2020-06-17 07:53:46 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+let x=document.getElementById('costumer-login')
+let y=document.getElementById('hotel-login')
+let z=document.getElementById('btn')
+
+function costumerlogin(){
+    console.log('kk')
+    x.style.left='-400px'
+    y.style.left='50px'
+    z.style.left='100px'
+}
+function hotellogin(){
+    x.style.left='50px'
+    y.style.left='450px'
+    z.style.left='0px'
+}
+
+// let a=document.getElementById('costumer-register')
+// let b=document.getElementById('hotel-register')
+// let c=document.getElementById('btn')
+// a.addEventListener('click',function costumerregister(){
+//     a.style.left='-400px'
+//     b.style.left='50px'
+//     c.style.left='100px'
+// })
+
+// y.addEventListenerfunction('click',function hotelregister(){
+//     a.style.left='50px'
+//     b.style.left='450px'
+//     c.style.left='0px'
+// })
