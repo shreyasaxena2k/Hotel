acd561073a8b0046c34d8cdbc0cf140fad068985
shreya saxena <shreya.saxena002@gmail.com>
2020-06-19 18:58:12 +0530

    files

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-let x=document.getElementById('costumer-login')
-let y=document.getElementById('hotel-login')
+let x=document.getElementById('clogin')
+let y=document.getElementById('hlogin')
 let z=document.getElementById('btn')
 
 function costumerlogin(){
-    console.log('kk')
-    x.style.left='-400px'
-    y.style.left='50px'
-    z.style.left='100px'
+    x.classList.remove('show')
+    y.classList.remove('show')
 }
 function hotellogin(){
-    x.style.left='50px'
-    y.style.left='450px'
-    z.style.left='0px'
+    y.classList.add('show')
+    x.classList.add('show')
+       
 }
 
-// let a=document.getElementById('costumer-register')
-// let b=document.getElementById('hotel-register')
-// let c=document.getElementById('btn')
-// a.addEventListener('click',function costumerregister(){
-//     a.style.left='-400px'
-//     b.style.left='50px'
-//     c.style.left='100px'
-// })
+let a=document.getElementById('cregister')
+let b=document.getElementById('hregister')
+let c=document.getElementById('btn')
+function customerregister(){ 
+    console.log(77)
+    a.classList.remove('show')
+    b.classList.remove('show')
+}
+function hotelregister(){
+    console.log(777)
+    
+    b.classList.add('show')
+    a.classList.add('show')
+}
 
-// y.addEventListenerfunction('click',function hotelregister(){
-//     a.style.left='50px'
-//     b.style.left='450px'
-//     c.style.left='0px'
-// })
